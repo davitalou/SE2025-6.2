81929bc91076628072c33af7b061c4db7d703916
biinternelmich1 <bi.intern.elmich@gmail.com>
2025-12-20 22:42:41 +0700

    fix hitbox

---
+++ Assets/Scripts/Editors/GraphicsSceneConfigEditor.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(DecoratingScene))]
+public class GraphicsSceneConfigEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        DecoratingScene scene = (DecoratingScene)target;
+
+        if (scene.config == null || scene.config.objects == null) return;
+    }
+
+    private void OnSceneGUI()
+    {
+        DecoratingScene scene = (DecoratingScene)target;
+        if (scene.config == null || scene.config.objects == null) return;
+
+        foreach (var obj in scene.config.objects)
+        {
+            if (obj.hitboxTriangles == null) continue;
+            foreach (var triangle in obj.hitboxTriangles)
+            {
+                Handles.color = Color.red;
+                Vector3[] points = {
+                    new Vector3(triangle.p1.x, triangle.p1.y, 0),
+                    new Vector3(triangle.p2.x, triangle.p2.y, 0),
+                    new Vector3(triangle.p3.x, triangle.p3.y, 0)
+                };
+                Handles.DrawAAConvexPolygon(points);
+
+                EditorGUI.BeginChangeCheck();
+                Vector2 newP1 = Handles.PositionHandle(new Vector3(triangle.p1.x, triangle.p1.y, 0), Quaternion.identity);
+                Vector2 newP2 = Handles.PositionHandle(new Vector3(triangle.p2.x, triangle.p2.y, 0), Quaternion.identity);
+                Vector2 newP3 = Handles.PositionHandle(new Vector3(triangle.p3.x, triangle.p3.y, 0), Quaternion.identity);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    triangle.p1 = newP1;
+                    triangle.p2 = newP2;
+                    triangle.p3 = newP3;
+                    EditorUtility.SetDirty(scene);
+                }
+            }
+        }
+    }
+}
